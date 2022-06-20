8edcda828a77b76bdd1d073c62df18d41fd4ea06
Fernando Sanchez <sanchezfernandogabriel@outlook.com>
2022-06-20 20:16:20 -0300

    navbar

---
+++ src/components/header/NavBar.js
@@ -0,0 +1,25 @@
+
+import logo from '../../imagenes/logo.png'
+
+
+const NavBar = () => {
+    return (
+        <>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="" class="brand-logo"><img src={logo} alt="logo" width={100} height={64}/>alma de acero</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">economico</a></li>
+        <li><a href="badges.html">medio</a></li>
+        <li><a href="collapsible.html">premium</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+            
+        </>
+    )
+
+    
+}
+export default NavBar
